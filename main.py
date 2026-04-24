7263204978caf1c9761a0df3a2d7949e19111b97
Diyang Raditya A <diyangraditya144@gmail.com>
2026-04-24 18:52:52 +0700

    feat: integrating openai service

--- main.py
+++ main.py
@@ -4,11 +4,14 @@ app = FastAPI()
 
 
 @app.get("/")
-def read_root():
+async def read_root():
     return {"Hello": "World"}
+
+@app.post("/api/v1/generate")
+async def generate():
+    return {"Content": "Generate"}
 
 
 if __name__ == "__main__":
     import uvicorn
-
     uvicorn.run(app, host="[IP_ADDRESS]", port=8000)
