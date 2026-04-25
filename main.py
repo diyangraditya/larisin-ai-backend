5a8980462a3b8a1818af016844d60d4a33bec172
Diyang Raditya A <diyangraditya144@gmail.com>
2026-04-25 18:40:07 +0700

    feat: progress developing the AI generation endpoint

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
+import os
+import requests
+
+from dotenv import load_dotenv
+
 from fastapi import FastAPI
+from openai import AzureOpenAI
 
 app = FastAPI()
 
+load_dotenv()
 
 @app.get("/")
 async def read_root():
@@ -9,7 +16,19 @@ async def read_root():
 
 @app.post("/api/v1/generate")
 async def generate():
-    return {"Content": "Generate"}
+    client = AzureOpenAI(
+        azure_endpoint=os.getenv("ENDPOINT_AZURE_OPENAI"),
+        api_key=os.getenv("OPENAI_AZURE_API"),
+        api_version="2024-05-01-preview",
+    )
+    response = client.chat.completions.create(
+        model=os.getenv("AZURE_MODEL"),
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": "Hello, how are you?"},
+        ],
+    )
+    return {"Content": response.choices[0].message.content}
 
 
 if __name__ == "__main__":
